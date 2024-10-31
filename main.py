3edd853533066850acb82dae2ed11d2873561f7f
JulianKep <132519348+JulianKep@users.noreply.github.com>
2024-10-31 19:22:02 +0100

    batch commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import ctypes
+import cv2
 from pathlib import Path
 
 #get path of dll relative to current directory
 lib_path = Path(__file__).parent / "clibrary.so"
+img_path = Path(__file__).parent / "monstera-blatt.jpg"
 
-print(__file__)
 
 #generate c-lib file with 'gcc -fPIC -shared -o clibrary.so clibrary.c'
 clibrary = ctypes.CDLL(str(lib_path))
@@ -15,3 +16,18 @@ clibrary.display(b"John", 18)
 
 print(clibrary.add(4, 5))
 print(clibrary.add(5, 6))
+
+
+
+
+
+
+image = cv2.imread(str(img_path))
+
+print(image)
+
+
+cv2.imshow("Monsterra", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
