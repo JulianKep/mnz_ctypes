fbdd3a3e65bb89293c1c4a45e518ebde3040a755
JulianKep <132519348+JulianKep@users.noreply.github.com>
2024-10-31 18:52:43 +0100

    batch commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-
 import ctypes
+from pathlib import Path
+
+#get path of dll relative to current directory
+lib_path = Path(__file__).parent / "clibrary.so"
+
+print(__file__)
 
 #generate c-lib file with 'gcc -fPIC -shared -o clibrary.so clibrary.c'
-clibrary = ctypes.CDLL("./clibrary.so")
+clibrary = ctypes.CDLL(str(lib_path))
 
 #binary string b because of c argument
 clibrary.display(b"John", 18)
 
 
 print(clibrary.add(4, 5))
+print(clibrary.add(5, 6))
