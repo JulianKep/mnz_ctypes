09b4e05f98dab2456921d1225e10f74fa67905c6
JulianKep <132519348+JulianKep@users.noreply.github.com>
2024-11-01 19:38:23 +0100

    batch commit

--- clibrary.c
+++ clibrary.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void display(char* str, int age){
 
-    printf("mynameis %s and my age is %d", str, age);
+
+
+
+//takes in 1D array and interprets it as 3D array, 1D array has to be in row major order
+void k_means(int* array, int rows, int cols, int value_array_length, int num_cluster_centers){
+
+    int total = rows * cols * value_array_length;
+    int row_length = cols * value_array_length;
+
+
+    //initialize random function
+    srand(918268612);
+    
+    //initialize k-means with random cluster centers
+    int middle[num_cluster_centers * 3];
+    for(int i=0; i<num_cluster_centers; i++){
+
+        int random = rand() % (total +1);
+
+    }
+
+    
+    //this loop iterates over rows by calculating the offset
+    for(int row_offset=0; row_offset<total; row_offset += cols*value_array_length){
+
+        for(int col_offset=0; col_offset<row_length; col_offset += value_array_length){
+
+            for(int i=0; i<3; i++){
+
+                printf("%d\n", array[row_offset + col_offset + i]);
+            }
+
+        }
+
+    }
+
+
 
 }
 
-int add(int a, int b){
-
-    return(a+b);
-}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ctypes
 import cv2
 from pathlib import Path
+import numpy as np
 
 #get path of dll relative to current directory
 lib_path = Path(__file__).parent / "clibrary.so"
@@ -10,21 +11,38 @@ img_path = Path(__file__).parent / "monstera-blatt.jpg"
 #generate c-lib file with 'gcc -fPIC -shared -o clibrary.so clibrary.c'
 clibrary = ctypes.CDLL(str(lib_path))
 
-#binary string b because of c argument
-#clibrary examples
-clibrary.display(b"John", 18)
-print(clibrary.add(4, 5))
-
-image = cv2.imread(str(img_path))
+#specify types that are handed to c-function by python
+clibrary.k_means.argtypes =(ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
+clibrary.k_means.restype = None
 
 
 
+""" image = cv2.imread(str(img_path)) """
 
+
+
+image = [[[4,5,6], [4,6,6], [4,7,6]],
+         [[3,5,6], [3,5,6], [3,5,6]],
+         [[2,5,6], [2,5,6], [2,5,6]],
+         [[1,5,6], [1,5,6], [1,5,6]]]
+
+image = np.array(image)
+
+rows, cols, value_array_length = image.shape
+
+
+# Convert the 3D array to a 1D array in row-major order to make it passable to c function
+image = image.ravel()
 
 print(image)
 
+#create array pointer
+image_ptr = image.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
 
-cv2.imshow("Monsterra", image)
+#call c function
+clibrary.k_means(image_ptr, rows, cols, value_array_length, num_cluster_centers)
+
+""" cv2.imshow("Monsterra", image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() """
 
